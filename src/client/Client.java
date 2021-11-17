0f5bd4b5e302d57a469b6053230e480bf79a621c
kostiya <kostiya@users.noreply.github.com>
2021-11-17 22:23:52 +0200

    Client login implemented. Probably need to refactor.

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,19 +10,40 @@ import java.util.Scanner;
 public class Client {
 
 	public Client() throws Exception {
-		
-		Socket socket = new Socket("127.0.0.1",2020);
+
+		Scanner keyboard = new Scanner (System.in);
+		System.out.println("Please Enter Server IP/Host:");
+		String hostOrIP = keyboard.nextLine();
+		System.out.println("Please Enter Server Port:");
+		int port = keyboard.nextInt();
+		Socket socket = new Socket(hostOrIP,port);
 		System.out.println("Successful connection to the server.");
 		
 		// I/O streams
 		BufferedReader in_socket = new BufferedReader (new InputStreamReader (socket.getInputStream()));
 		PrintWriter out_socket = new PrintWriter (new OutputStreamWriter (socket.getOutputStream()), true);
-		Scanner keyboard = new Scanner (System.in);
 
-		String message = in_socket.readLine();
-		System.out.println("Server says: " + message);
-		message = keyboard.nextLine();
-		out_socket.println(message);
+		System.out.println("Please Enter Your Username from the list:");
+		String userInList;
+		do{
+			userInList = in_socket.readLine();
+			if(!userInList.equals("\\n")){
+				System.out.println(userInList);
+			} else {
+				break;
+			}
+		}while(1==1);
+
+		String username = keyboard.nextLine();
+		out_socket.println(username);
+		String answer_message = in_socket.readLine();
+
+		while(!answer_message.equals("Connected")){
+			System.out.println(answer_message);
+			username = keyboard.nextLine();
+			out_socket.println(username);
+			answer_message = in_socket.readLine();
+		}
 		
 		socket.close();
 		System.out.println("Socket closed.");
